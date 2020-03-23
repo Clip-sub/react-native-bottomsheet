1868475cee4ea329c7b7f751d5033622ddfcbc93
vuongpsht <34977724+vuongpsht@users.noreply.github.com>
2020-03-23 15:00:01 +0700

    type declare

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+export interface showBottomSheetWithOptionsDTO {
+  options?: string[]
+  title?: string
+  dark?: boolean
+  cancelButtonIndex?: number
+}
+
+export interface showBottomSheetWithOptionsCallBackDTO {
+  callBack?: (value: number) => void
+}
+
+export interface showShareBottomSheetWithOptionsDTO {
+  url: string
+  subject: string
+  message: string
+}
+
+export default class BottomSheet {
+  static showBottomSheetWithOptions: (
+    options?: showBottomSheetWithOptionsDTO,
+    callback?: showBottomSheetWithOptionsCallBackDTO,
+  ) => void
+  static showShareBottomSheetWithOptions: (
+    options: showShareBottomSheetWithOptionsDTO,
+    callBack: (value: any) => void,
+  ) => void
+}
